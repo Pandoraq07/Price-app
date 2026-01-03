15b0428768a467776f87352ba538917ed38848db
Pandoraq07 <pandoraking68@gmail.com>
2026-01-03 17:36:36 +0100

    Upgrade

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const amountInput = document.getElementById("amount");
-const discountSelect = document.getElementById("discount-percentage");
 const discountAmountInput = document.getElementById("discount-amount");
+const couponInput = document.getElementById("coupon-input");
 const finalPayInput = document.getElementById("pay");
 const form = document.querySelector("form");
+
+const coupons = [
+    {name: "HELLO5", discount: 5},
+    {name: "SAVE10", discount: 10},
+    {name: "MIXED15", discount: 15},
+    {name: "DIVE20", discount: 20},
+    {name: "MINE25", discount: 25},
+
+];
 
 const nairaFormatter = new Intl.NumberFormat('en-NG', {
     style: 'currency',
@@ -12,11 +21,18 @@ const nairaFormatter = new Intl.NumberFormat('en-NG', {
 
 const updateValues = () => {
     const amount = Number(amountInput.value);
-    const percentage = Number(discountSelect.value);
+    const userCode = couponInput.value.trim().toUpperCase();
 
     if (isNaN(amount) || amount <= 0) {
         alert("Please enter a valid amount greater than 0");
         return;
+    }
+const foundCoupon = coupons.find(c => c.name === userCode);
+    let percentage = 0;
+    if(foundCoupon){
+        percentage = foundCoupon.discount;
+    }else if (userCode !== ""){
+        alert("Invalid Coupon Code")
     }
 
     const discount = (amount * percentage) / 100;
